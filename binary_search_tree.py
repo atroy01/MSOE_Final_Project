f79db0f2f88e7ab109b462aaa0784fe03c5c713f
Andrew Troy <atroy01@gmail.com>
2023-08-09 23:38:45 -0500

    added getCountryList to create list to be accessed by UI

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -2,7 +2,6 @@
 # Andrew Troy
 # 2023-08-09
 
-
 class Country(object):  # One part of the data Tree
     def __init__(self, population: int, name: str, other_data=[]):  # Constructor
         self.population = population  # the population of the country. This is the key for the BST
@@ -29,15 +28,15 @@ class PopulationNode(object):  # One part of the data Tree
 
 class PopulationBST(object):  # A binary search tree of data elements
     def __init__(self):  # Constructor
-        self.__root = None  # Reference to start of BST
+        self.root = None  # Reference to start of BST
         self.size = 0  # variable to keep track of the size of the tree
 
     def addCountry(self, country: Country):
         self.size += 1
-        if not self.__root:
-            self.__root = PopulationNode(country.population, [country])  # if no root exists, then create a new PopulationNode with one country in it
+        if not self.root:
+            self.root = PopulationNode(country.population, [country])  # if no root exists, then create a new PopulationNode with one country in it
         else:
-            self._addCountry(self.__root, country)
+            self._addCountry(self.root, country)
         # # TODO: I think here we need to simultaneously add a country to the CountryBST
         # countryBST.addCountry(country)
         return
@@ -58,10 +57,10 @@ class PopulationBST(object):  # A binary search tree of data elements
 
     def findCountryFromPopulation(self, population: int):
         print(f'Looking for country with population of {population}.')
-        if not self.__root:
+        if not self.root:
             print('No tree exists')
         else:
-            self._findCountryFromPopulation(self.__root, population, None, None)
+            self._findCountryFromPopulation(self.root, population, None, None)
         return
 
     def _findCountryFromPopulation(self, cur: PopulationNode, population: int, lastLeft: PopulationNode, lastRight: PopulationNode,):
@@ -92,31 +91,21 @@ class PopulationBST(object):  # A binary search tree of data elements
             self.printLargestToSmallest(cur.left)
 
     def __str__(self):
-        self.printLargestToSmallest(self.__root)
+        self.printLargestToSmallest(self.root)
         return ''
 
-# class CountryNode(object):  # One part of the data Tree
-#     def __init__(self, country: Country):  # Constructor
-#         self.name = name  # the name of the country. This is the key for the CountryBST
-#         self.left = None  # the left child
-#         self.right = None  # the right child
-#
-#     def __str__(self):
-#         population = self.countries[0].population  # get the population of one of the countries in the countries list
-#         country_names = ', '.join(country.name for country in self.countries)
-#         return f'{population}: {country_names}'
 
 class CountryBST(object):  # A binary search tree of data elements
     def __init__(self):  # Constructor
-        self.__root = None  # Reference to start of BST
+        self.root = None  # Reference to start of BST
         self.size = 0  # variable to keep track of the size of the tree
 
     def addCountry(self, country: Country):  # this is the starter form to check if the roo exists
         self.size += 1
-        if not self.__root:
-            self.__root = country  # if no root exists, then make the passed country the root
+        if not self.root:
+            self.root = country  # if no root exists, then make the passed country the root
         else:
-            self._addCountry(self.__root, country)  # enter the recursive function
+            self._addCountry(self.root, country)  # enter the recursive function
         return
 
     def _addCountry(self, cur: Country, countryToAdd: Country):  # this is the recursive form of the function
@@ -135,10 +124,10 @@ class CountryBST(object):  # A binary search tree of data elements
 
     def findPopulationFromCountry(self, name: str):
         print(f'Looking for population of country {name}.')
-        if not self.__root:
+        if not self.root:
             print('   No tree exists')
         else:
-            self._findPopulationFromCountry(self.__root, name)
+            self._findPopulationFromCountry(self.root, name)
         return
 
     def _findPopulationFromCountry(self, cur: Country, name: str):
@@ -158,15 +147,17 @@ class CountryBST(object):  # A binary search tree of data elements
             print(f'   The population is {cur.population}')
             return
 
-    def printLargestToSmallest(self, cur: Country):
+    def printLargestToSmallest(self, cur: Country, countryList=[]):
         if cur.left:
-            self.printLargestToSmallest(cur.left)
+            self.printLargestToSmallest(cur.left, countryList)
         print(f'{cur}: {cur.population}')
+        countryList.append(cur.name)
         if cur.right:
-            self.printLargestToSmallest(cur.right)
+            self.printLargestToSmallest(cur.right, countryList)
+        return countryList
 
     def __str__(self):
-        self.printLargestToSmallest(self.__root)
+        self.printLargestToSmallest(self.root, [])
         return ''
 
 
@@ -184,11 +175,21 @@ class BST_Controller(object):
     def findCountryFromPopulation(self, population: int):
         self.bst_p.findCountryFromPopulation(population)
 
+
     def findPopulationFromCountry(self, name: str):
         self.bst_c.findPopulationFromCountry(name)
+
 
     def printByPopulation(self):
         print(self.bst_p)
 
     def printByCountry(self):
         print(self.bst_c)
+        return
+
+
+    def getCountryList(self):
+        if not self.bst_c.root:
+            return ['No Countries In Database']
+        else:
+            return self.bst_c.printLargestToSmallest(self.bst_c.root, [])
